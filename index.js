b93d8d656154dd8963c266950fd55dfbc1edb334
luis-luciqm <redbullfc18@gmail.com>
2021-05-04 16:56:32 -0300

    Iniciando front

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 
 // app.use(express.static(__dirname + '/css'));
 
+app.use(express.static(__dirname + '/src/js'));
+app.use(express.static(__dirname + '/src/css'));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/src/index.html")
 })
@@ -10,10 +13,6 @@ app.get("/", (req, res) => {
 app.get("/listaContatos", (req, res) => {
     res.send("Contatos Disponíveis")
 })
-
-app.use(express.static(__dirname + '/src/js'));
-app.use(express.static(__dirname + '/src/css'));
-console.log(__dirname + '/src/css');
 
 app.listen(5000, () => {
     console.log('Server is running port 5000')
